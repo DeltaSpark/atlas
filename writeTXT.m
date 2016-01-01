75c7de6127eaa63b7aa3914360b57c8b88ba819b
Andy Chu <vox@andychu.me>
2016-01-01 15:55:06 -0800

    style: Add commenting to writeTXT script

--- writeTXT.m
+++ writeTXT.m
@@ -1,39 +1,49 @@
-% Include a waitbar for progress monitoring
+% MATLAB Script file that will convert fences and impressions matricies to
+% Javascript arrays for use by the html/js viewer
 
+% Load fences and impressions from .mat file
+load fences.mat
+load impressions.mat
+
+% Draw a waitbar for progress monitoring
 h = waitbar(0,'Writing fences to javascript file...');
 
+% Save length of fences and impressions matricies
 num_fences = length(fences);
 num_impressions = length(impressions);
 
+% Save total loop iterations for waitbar
 total_progress = num_fences + num_impressions;
 
-% Write Fences to txt file
+% Write fences matrix as a minified js file
 fenceFileID = fopen('fences.js','w');
 fprintf(fenceFileID,'var fences=[');
 
 for i = 1:num_fences
     fprintf(fenceFileID,'[%f,%f,%d],',fences(i,1),fences(i,2),fences(i,3));
-    
     waitbar(i / total_progress, h)
 end
 
 fprintf(fenceFileID,'];');
 fclose(fenceFileID);
 
-
-% Write Impressions to txt file
+% Write impressions matrix as a minified js file
 impressionsFileID = fopen('impressions.js','w');
 fprintf(impressionsFileID,'var impressions=[');
 
+% Update waitbar message with transition to impressions
 waitbar(num_fences / total_progress, h,'Writing impressions to javascript file...')
 
 for i = 1:num_impressions
     fprintf(impressionsFileID,'[%f,%f],',impressions(i,1),impressions(i,2));
-    
     waitbar((num_fences + i) / total_progress, h)
 end
 
 fprintf(impressionsFileID,'];');
 fclose(impressionsFileID);
 
+% Close waitbar
 close(h)
+
+% Garbage collection to remove variables local to script
+clear fenceFileID i impressionsFileID num_fences num_impressions total_progress h
