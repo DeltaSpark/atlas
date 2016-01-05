4898b24664367ffc6778b540a3960676b3b2f858
Andy Chu <vox@andychu.me>
2016-01-04 21:53:29 -0800

    feat: Add clustering algorithm to resolve issue #1

--- writeTXT.m
+++ writeTXT.m
@@ -5,45 +5,71 @@
 load fences.mat
 load impressions.mat
 
-% Draw a waitbar for progress monitoring
-h = waitbar(0,'Writing fences to javascript file...');
+% Debug: Notification that geofence processing has begun
+disp('Processing geofences...');
 
-% Save length of fences and impressions matricies
-num_fences = length(fences);
-num_impressions = length(impressions);
-
-% Save total loop iterations for waitbar
-total_progress = num_fences + num_impressions;
-
-% Write fences matrix as a minified js file
-fenceFileID = fopen('fences.js','w');
-fprintf(fenceFileID,'var fences=[');
-
-for i = 1:num_fences
-    fprintf(fenceFileID,'[%f,%f,%d],',fences(i,1),fences(i,2),fences(i,3));
-    waitbar(i / total_progress, h)
+% Convert fence coordinates into strings
+for i = 1:length(fences)
+    stringFences(i) = {sprintf('[%f,%f,%d]',fences(i,1),fences(i,2),fences(i,3))};
 end
 
-fprintf(fenceFileID,'];');
+% Write fence strings as a minified js file
+s = strcat('var fences=[',strjoin(stringFences,','),'];');
+
+fenceFileID = fopen('fences.js','w');
+fprintf(fenceFileID,s);
 fclose(fenceFileID);
 
-% Write impressions matrix as a minified js file
-impressionsFileID = fopen('impressions.js','w');
-fprintf(impressionsFileID,'var impressions=[');
+% Garbage collection to remove variables local to script
+clear i fenceFileID s stringFences
+
+% Debug: Notification that impression processing has begun
+disp('Processing impressions...');
+
+% Setup histogram to include bins every .005 degrees (approx 56 meters) and
+% to count the number of impressions in each bin
+
+% Defines number of bins in the X and Y axis (or lat, lon)
+% binX = (max latitude - min latitude) / .005, rounded to nearest integer
+% binY = (max long - min long) / .005, rounded to nearest integer
+binX = 1391; binY = 1794;
+
+% Count the number of impressions in each bin, including duplicates
+[N,Xedges,Yedges] = histcounts2(impressions(:,1),impressions(:,2),[binX binY]);
+numClusters = nnz(N);
+
+% Debug: Displays the number of bins filled with at least 1 impression
+fprintf('Impressions clustered into %d markers.\n',numClusters);
+
+% Debug: Notification that impression processing has begun
+disp('Extracting nonzero elements from the clustered data...');
+
+% Find nonzero elements in the histogram
+[rowIndex, colIndex, values] = find(N);
 
 % Update waitbar message with transition to impressions
-waitbar(num_fences / total_progress, h,'Writing impressions to javascript file...')
+h = waitbar(0,'Converting clustered data into marker strings...');
 
-for i = 1:num_impressions
-    fprintf(impressionsFileID,'[%f,%f],',impressions(i,1),impressions(i,2));
-    waitbar((num_fences + i) / total_progress, h)
+% Extract values of nonzero elements and convert into strings
+for j = 1:numClusters
+    stringClusters(j) = {sprintf('[%.3f,%.3f,%d]',Xedges(rowIndex(j)), Yedges(colIndex(j)), values(j))};
+    debugClusterMatrix(j,:) = [Xedges(rowIndex(j)), Yedges(colIndex(j)), values(j)];
+    
+    waitbar(j / numClusters, h)
 end
-
-fprintf(impressionsFileID,'];');
-fclose(impressionsFileID);
 
 % Close waitbar
 close(h)
 
+% Debug: Notification that impression processing has begun
+disp('Writing data to file...');
+
+% Write impression cluster strings as a minified js file
+s = strcat('var impressions=[',strjoin(stringClusters,','),'];');
+
+impressionsFileID = fopen('impressions.js','w');
+fprintf(impressionsFileID,s);
+fclose(impressionsFileID);
+
 % Garbage collection to remove variables local to script
-clear fenceFileID i impressionsFileID num_fences num_impressions total_progress h
+clear j impressionsFileID h binX binY N Xedges Yedges colIndex rowIndex values s numClusters stringClusters
